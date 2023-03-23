d7a259b2b405ca0f4b4caa8854fd80c1dd2b8378
Yuseop <dbtjq1592000@gmail.com>
2023-03-23 12:42:55 +0900

    [Silver III] Title: N과 M (3), Time: 572 ms, Memory: 208988 KB -BaekjoonHub

---
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.java
@@ -0,0 +1,59 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+	static int[] arr;
+	static int[] result;
+	static boolean[] visited;
+	static int N;
+	static int M;
+	static StringBuilder sb = new StringBuilder();
+	
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+
+		arr = new int[N];
+		result = new int[M];
+		visited = new boolean[N];
+		
+		for(int i = 0; i < N; i++) {
+			arr[i] = i+1;
+		}
+		
+		perm(0);
+		System.out.println(sb);
+	}
+
+	private static void perm(int idx) {
+		// 종료조건
+		if (idx == M) {
+			for(int i = 0; i<M; i++) {
+				sb.append(result[i]+" ");
+			}
+			sb.append("\n");
+			return;
+		}
+
+		// 반복조건
+		for(int i = 0; i < N; i++) {
+			if(visited[i]) continue;
+			
+			result[idx] = arr[i];
+			
+			
+			perm(idx+1);
+			
+			
+		}
+		
+	}
+
+}
