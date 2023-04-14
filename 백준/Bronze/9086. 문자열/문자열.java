0b991835c78f24bb6c6054afdd7ce5a06c5dee4a
Yuseop <dbtjq1592000@gmail.com>
2023-04-14 10:06:41 +0900

    [Bronze V] Title: 문자열, Time: 152 ms, Memory: 15884 KB -BaekjoonHub

---
+++ 백준/Bronze/9086. 문자열/문자열.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int T = Integer.parseInt(br.readLine());
+		
+		for(int tc = 0; tc < T; tc++) {
+			String s = br.readLine();
+			
+			System.out.println(s.charAt(0)+""+s.charAt(s.length()-1));
+			
+		}
+		
+		
+	}
+
+}
