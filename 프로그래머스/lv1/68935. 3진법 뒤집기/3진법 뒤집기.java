a9caadd1adaee4ae187fda165ba80c56874bd5ec
Yuseop <dbtjq1592000@gmail.com>
2023-09-12 08:30:37 +0900

    [level 1] Title: 3진법 뒤집기, Time: 1.71 ms, Memory: 88.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/68935. 3진법 뒤집기/3진법 뒤집기.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        String s = "";
+        
+        while(n != 0) {
+            s += String.valueOf(n%3);
+            n = n / 3;
+        }
+        
+        
+        for(int i = 0; i < s.length(); i++) {
+            answer += Math.pow(3,i)*(s.charAt(s.length()-i-1)-'0');
+            System.out.println(answer);
+        }
+        
+        return answer;
+    }
+}
