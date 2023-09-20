2b12d33ce8e014aa1b2750c45fbc968b1971a167
Yuseop <dbtjq1592000@gmail.com>
2023-09-21 00:47:23 +0900

    [level 2] Title: 주식가격, Time: 9.76 ms, Memory: 61.1 MB -BaekjoonHub

--- 프로그래머스/lv2/42584. 주식가격/주식가격.java
+++ 프로그래머스/lv2/42584. 주식가격/주식가격.java
@@ -1,21 +1,15 @@
 class Solution {
     public int[] solution(int[] prices) {
-        int[] answer = new int[prices.length];
-        
-        for(int i = 0; i < prices.length-1; i++) {
-            for(int j = i+1; j < prices.length; j++) {
-                if(prices[i] > prices[j]) {
+        int len = prices.length;
+        int[] answer = new int[len];
+        int i, j;
+        for (i = 0; i < len; i++) {
+            for (j = i + 1; j < len; j++) {
+                answer[i]++;
+                if (prices[i] > prices[j])
                     break;
-                    
-                } else {
-                    answer[i]++;
-                }
             }
-            
-            
         }
-        
-        
         return answer;
     }
 }
