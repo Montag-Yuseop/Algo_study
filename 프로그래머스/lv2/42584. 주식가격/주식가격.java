d93727c4779f8cfb88796838712bad1f26f46a4b
Yuseop <dbtjq1592000@gmail.com>
2023-09-21 00:38:39 +0900

    [level 2] Title: 주식가격, Time: 30.38 ms, Memory: 78.3 MB -BaekjoonHub

--- 프로그래머스/lv2/42584. 주식가격/주식가격.java
+++ 프로그래머스/lv2/42584. 주식가격/주식가격.java
@@ -1,47 +1,21 @@
-import java.util.*;
-
 class Solution {
-    static class Node {
-        int time, value;
-        
-        public Node(int time, int value) {
-            
-            this.time = time;
-            this.value = value;
-            
-        }
-        
-    }
-    
-    
     public int[] solution(int[] prices) {
         int[] answer = new int[prices.length];
-        Stack<Node> stack = new Stack();
         
-        // 스택 문제?
-        
-        for(int i = 0; i < prices.length; i++) {
-            if(stack.isEmpty() || stack.peek().value <= prices[i]) {
-                stack.add(new Node(i, prices[i])); // 스택이 비어있거나
-            } else {
-                Node cur = stack.pop();
-                answer[cur.time] = i - cur.time;
-                while(!stack.isEmpty() && stack.peek().value > prices[i]) {
-                    cur = stack.pop();
-                    answer[cur.time] = i - cur.time;
+        for(int i = 0; i < prices.length-1; i++) {
+            for(int j = i+1; j < prices.length; j++) {
+                if(prices[i] > prices[j]) {
+                    break;
+                    
+                } else {
+                    answer[i]++;
                 }
-                stack.add(new Node(i, prices[i]));
             }
-
-        }
-        
-        while(!stack.isEmpty()) {
-            Node cur = stack.pop();     
-            answer[cur.time] = prices.length - cur.time-1;
+            
             
         }
         
-                
+        
         return answer;
     }
 }
