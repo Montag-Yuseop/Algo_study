445b7c748709284f345b8218336898016c71cb90
Yuseop <dbtjq1592000@gmail.com>
2024-01-18 01:29:08 +0900

    [Gold V] Title: 옥상 정원 꾸미기, Time: 300 ms, Memory: 23664 KB -BaekjoonHub

--- 백준/Gold/6198. 옥상 정원 꾸미기/옥상 정원 꾸미기.java
+++ 백준/Gold/6198. 옥상 정원 꾸미기/옥상 정원 꾸미기.java
@@ -8,23 +8,22 @@ public class Main {
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
+        int N = Integer.parseInt(br.readLine());
+        long answer = 0;
+        Stack<Integer> stk = new Stack<>();
 
-        long result = 0;
-        Stack<Integer> s = new Stack<>();
-        for (int i = 0; i < n; i++) {
-            int tmp = Integer.parseInt(br.readLine());
-            while (!s.isEmpty() && s.peek() <= tmp) {
-                s.pop();
+        for(int i = 0; i < N; i++) {
+            int now = Integer.parseInt(br.readLine());
+
+            while(!stk.isEmpty() && stk.peek() <= now) {
+                stk.pop();
             }
-            s.push(tmp);
-//            System.out.println(s.size()-1);
-            result += s.size() - 1;
 
+            stk.push(now);
+            answer += stk.size()-1;
         }
 
-        System.out.println(result);
-
+        System.out.println(answer);
 
     }
 
