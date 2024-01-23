ded38e4f7676109ead50cb2d0d630fc66f014fd3
Yuseop <dbtjq1592000@gmail.com>
2024-01-23 17:14:05 +0900

    [Bronze III] Title: 홀수, Time: 120 ms, Memory: 14164 KB -BaekjoonHub

---
+++ 백준/Bronze/2576. 홀수/홀수.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int answer = 0;
+        int odd = Integer.MAX_VALUE;
+
+        for(int i = 0; i < 7; i++) {
+            int N = Integer.parseInt(br.readLine());
+
+            if(N % 2 != 0) {
+                answer += N;
+                odd = Math.min(odd, N);
+            }
+        }
+
+        if(odd == Integer.MAX_VALUE) System.out.println(-1);
+        else {
+            System.out.println(answer);
+            System.out.println(odd);
+        }
+
+    }
+
+}
