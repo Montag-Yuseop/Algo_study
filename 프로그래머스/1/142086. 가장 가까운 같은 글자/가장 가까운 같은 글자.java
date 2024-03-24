ddc009d68ec53a6212c03672f612be0487371e09
Yuseop <dbtjq1592000@gmail.com>
2024-03-24 12:36:37 +0900

    [level 1] Title: 가장 가까운 같은 글자, Time: 2.74 ms, Memory: 84.8 MB -BaekjoonHub

--- 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.java
+++ 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.java
@@ -1,26 +1,6 @@
-import java.util.*;
-
 class Solution {
     public int[] solution(String s) {
-        int[] answer = new int[s.length()];
-        Map<Character, Integer> map = new HashMap<>();
-        
-        for(int i = 0; i < s.length(); i++) {
-            char c = s.charAt(i);
-            
-            if(map.get(c) == null) {
-                answer[i] = -1;
-                map.put(c, i);
-            } else {
-                int before = map.get(c);
-                answer[i] = i - before;
-                map.put(c, i);
-            }
-            
-            
-        }
-        
-        
+        int[] answer = {};
         return answer;
     }
 }
