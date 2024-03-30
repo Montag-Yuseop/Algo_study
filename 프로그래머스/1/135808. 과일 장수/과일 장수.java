53d0c705bf6b08e6ac50dfc46f4f909a691e073f
Yuseop <dbtjq1592000@gmail.com>
2024-03-30 17:46:57 +0900

    [level 1] Title: 과일 장수, Time: 0.48 ms, Memory: 73.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/135808. 과일 장수/과일 장수.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int k, int m, int[] score) {
+        int answer = 0;
+        
+        Arrays.sort(score);
+        int idx = score.length-1;
+        while(true) {
+            idx = idx - m + 1;
+            if(idx < 0) break;
+            
+            answer += (score[idx] * m);
+            idx -= 1;
+                
+            
+            
+            
+            
+            
+            
+        }
+        
+        
+        return answer;
+    }
+}
