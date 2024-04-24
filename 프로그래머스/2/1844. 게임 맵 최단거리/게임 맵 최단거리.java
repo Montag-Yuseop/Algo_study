dfe754f6dded8b7095004f63c21e6c549e4d7a7e
Yuseop <dbtjq1592000@gmail.com>
2024-04-24 16:16:49 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 7.85 ms, Memory: 54.5 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,37 +1,33 @@
 import java.util.*;
 
 class Solution {
-    
     static class Node {
-        int r, c, cnt;
+        int r, c, value;
         
-        public Node(int r, int c, int cnt) {
+        public Node(int r, int c, int value) {
             this.r = r;
             this.c = c;
-            this.cnt = cnt;
+            this.value = value;
         }
+        
     }
-    
-    static boolean[][] visited;
-    static int N, M, answer;
+    static int answer = -1;
     static int[] dr = {0, 0, -1, 1};
     static int[] dc = {-1, 1, 0, 0};
     
-    
     public int solution(int[][] maps) {
-        answer = Integer.MAX_VALUE;
-        N = maps.length;
-        M = maps[0].length;
-        visited = new boolean[N][M];
         
         bfs(maps);
         
-        if(answer == Integer.MAX_VALUE) return -1;
         return answer;
     }
     
-    static void bfs(int[][] maps) {
-        Queue<Node> q = new LinkedList();
+    static void bfs(int[][] map) {
+        Queue<Node> q = new LinkedList<>();
+        int targetR = map.length - 1;
+        int targetC = map[0].length - 1;
+        boolean[][] visited = new boolean[targetR + 1][targetC + 1];
+        
         q.offer(new Node(0, 0, 1));
         visited[0][0] = true;
         
@@ -39,30 +35,32 @@ class Solution {
             Node cur = q.poll();
             int nowR = cur.r;
             int nowC = cur.c;
-            int nowCnt = cur.cnt;
+            int nowValue = cur.value;
             
-            if(nowR == N-1 && nowC == M-1) {
-                answer = nowCnt;
-                return;
+            if(nowR == targetR && nowC == targetC) {
+                answer = nowValue;
             }
             
             for(int d = 0; d < 4; d++) {
                 int nextR = nowR + dr[d];
                 int nextC = nowC + dc[d];
-                int nextCnt = nowCnt + 1;
                 
-                if(bc(nextR, nextC) && maps[nextR][nextC] == 1 && !visited[nextR][nextC]) {
+                if(bc(nextR, nextC, targetR + 1, targetC + 1) &&
+                   map[nextR][nextC] == 1 && 
+                   !visited[nextR][nextC]) {
                     visited[nextR][nextC] = true;
-                    q.offer(new Node(nextR, nextC, nextCnt));
+                    q.offer(new Node(nextR, nextC, nowValue + 1));
                 }
                 
             }
             
         }
+        
+        
     }
     
-    static boolean bc(int r, int c) {
-        return r >= 0 && r < N && c >= 0 && c < M;
+    static boolean bc(int r, int c, int N, int M) {
+        return r >= 0 && c >= 0 && r < N && c < M;
     }
     
 }
