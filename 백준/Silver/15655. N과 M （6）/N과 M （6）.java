6714e2a13ce14d14807eac9f89dcc91be1a788b3
Yuseop <dbtjq1592000@gmail.com>
2024-05-08 17:09:09 +0900

    [Silver III] Title: N과 M (6), Time: 80 ms, Memory: 11580 KB -BaekjoonHub

---
+++ 백준/Silver/15655. N과 M （6）/N과 M （6）.java
@@ -0,0 +1,56 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static boolean[] visited;
+    static int[] arr, map;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        visited = new boolean[N + 1];
+        arr = new int[M];
+        map = new int[N];
+
+        st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < N; i++) {
+            map[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(map);
+        dfs(0, 0, N, M);
+
+        System.out.println(sb);
+    }
+
+    static void dfs(int num, int depth, int N, int M) {
+        if(depth == M) {
+            for(int i : arr) {
+                sb.append(i).append(" ");
+            }
+
+            sb.append("\n");
+            return;
+        }
+
+        for(int i = num; i < N; i++) {
+            if(!visited[i]) {
+                visited[i] = true;
+                arr[depth] = map[i];
+                dfs(i, depth + 1, N, M);
+                visited[i] = false;
+            }
+
+        }
+
+    }
+
+}
