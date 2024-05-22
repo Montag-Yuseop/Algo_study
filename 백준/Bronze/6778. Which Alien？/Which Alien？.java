efec43c36feed40d9b937b924923a41b69624da2
Yuseop <dbtjq1592000@gmail.com>
2024-05-23 01:14:55 +0900

    [Bronze IV] Title: Which Alien?, Time: 112 ms, Memory: 12808 KB -BaekjoonHub

---
+++ 백준/Bronze/6778. Which Alien？/Which Alien？.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int antena = sc.nextInt();
+        int eyes = sc.nextInt();
+
+
+        if (antena >= 3 && eyes <=4){
+            System.out.println("TroyMartian");
+        }if(antena <= 6 && eyes >= 2 ){
+            System.out.println("VladSaturnian");
+        }if(antena <=2 && eyes <= 3){
+            System.out.println("GraemeMercurian");
+        }else {
+
+        }
+
+    }
+}
