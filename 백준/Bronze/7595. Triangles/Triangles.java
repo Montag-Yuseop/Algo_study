23163cb983e64ed6def2a31b5f09bf1150a815da
Yuseop <dbtjq1592000@gmail.com>
2024-05-28 10:57:06 +0900

    [Bronze IV] Title: Triangles, Time: 184 ms, Memory: 17960 KB -BaekjoonHub

--- 백준/Bronze/7595. Triangles/Triangles.java
+++ 백준/Bronze/7595. Triangles/Triangles.java
@@ -1,19 +1,21 @@
-import java.util.Scanner;
+import java.util.*;
 public class Main {
 	public static void main(String[] args) {
 		
 		Scanner sc = new Scanner(System.in);
+        StringBuilder sb = new StringBuilder();
 		
 		int a = sc.nextInt();
 		while(a != 0) {
 			for(int i = 1; i <= a; i++) {
 				for(int j = 1; j <= i; j++) {
-					System.out.print("*");
+					sb.append("*");
 				}
-				System.out.println();
+				sb.append("\n");
 			}
 			a = sc.nextInt();
 		}
 		sc.close();
+        System.out.println(sb);
 	}
 }
