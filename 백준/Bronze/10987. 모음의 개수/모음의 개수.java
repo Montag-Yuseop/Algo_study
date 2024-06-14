e8ee4ab2b8220d78ded0da8779f6835de2fbf187
Yuseop <dbtjq1592000@gmail.com>
2024-06-14 17:38:13 +0900

    [Bronze IV] Title: 모음의 개수, Time: 80 ms, Memory: 11500 KB -BaekjoonHub

---
+++ 백준/Bronze/10987. 모음의 개수/모음의 개수.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int ans = 0;
+        String s = br.readLine();
+        
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u') ans++;
+        }
+        System.out.print(ans);
+    }
+}
