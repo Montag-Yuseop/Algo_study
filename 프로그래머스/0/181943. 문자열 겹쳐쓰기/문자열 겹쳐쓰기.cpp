ca151adf3e4775a4bf80f76576efe27079724a19
Yuseop <dbtjq1592000@gmail.com>
2024-07-08 23:01:21 +0900

    [level 0] Title: 문자열 겹쳐쓰기, Time: 0.01 ms, Memory: 3.62 MB -BaekjoonHub

---
+++ 프로그래머스/0/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.cpp
@@ -0,0 +1,15 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string my_string, string overwrite_string, int s) {
+    string answer = "";
+    
+    answer = my_string.substr(0, s) + overwrite_string.substr(0, overwrite_string.length())
+        + my_string.substr(s + overwrite_string.length(), my_string.length());
+        
+        
+    
+    return answer;
+}
